b446a400a0f8148b793d29f1fd5855c703b70c69
gnshvsk <40975528+gnshvsk@users.noreply.github.com>
2019-01-12 08:29:58 +0400

    Add files via upload

---
+++ FileManager.java
@@ -0,0 +1,54 @@
+﻿package lab4;
+
+import java.io.*;
+
+
+public class FileManager {
+
+    
+    public void copyFile (String src, String dst) throws IOException{
+        
+        InputStream is = null;
+        
+        OutputStream os= null;
+
+        
+        int size=1*1024*1024;//Size 1Mb
+        
+        long startTime = System.nanoTime();
+        
+        try{
+            
+             is = new FileInputStream(src);
+            
+             os = new FileOutputStream(dst);
+            
+             byte[] buffer = new byte [size];
+            
+             int length;
+            
+             while ((length = is.read(buffer)) >= 0){
+                
+                 os.write(buffer,  0, length);
+            
+             }
+        
+        }
+        
+        finally{
+            
+            is.close();
+
+            os.close();
+        
+        }
+        
+        long endTime=System.nanoTime();
+        
+        System.out.print("Процесс копирования занял: ");
+        
+        System.out.print(endTime-startTime);
+
+        System.out.println(" наносекунд");
+    }
+}
---
+++ Lab4.java
@@ -0,0 +1,34 @@
+﻿package Lab4;
+
+public class Lab4 {
+
+  
+    
+    public static void main(String[] args) {
+        
+    String src = "C:\Users\Admin\Documents\text.txt";
+        
+    String dst = "C:\Users\Admin\Documents\newtext.txt";
+        
+    FileManager fileManager;
+        
+    try{
+            
+        fileManager =new FileManager();
+            
+        fileManager.copyFile(src,dst);
+            
+        System.out.println("Копирование прошло успешно");
+        
+         }
+        
+    catch (Exception e){
+            
+        System.out.println(e.getMessage());
+    
+         }
+    
+
+    }
+
+}
